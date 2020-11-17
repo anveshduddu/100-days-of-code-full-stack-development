6f3f652e6a8dc8ab6e36dbf9489e22539898e485
anvesh duddu <anvesh81255@gmail.com>
2020-11-17 12:28:54 +0530

    day1

---
+++ Day1/Day1.js
@@ -0,0 +1,20 @@
+
+function myFunction() {
+  alert("Day 1");
+}
+
+var countDownDate = new Date("Feb 24, 2021 23:59:59").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);

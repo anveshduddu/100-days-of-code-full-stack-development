92d652ebd915b56ea950fdb73a97fbd3924b22bb
anvesh duddu <37796863+anveshduddu@users.noreply.github.com>
2021-03-29 02:36:22 +0530

    day93

---
+++ Day93/day93.js
@@ -0,0 +1,13 @@
+const op = setInterval(incNum, 20);
+function incNum() {
+    const text = document.getElementById("text");
+    const line = document.getElementById("line");
+
+    let a = window.getComputedStyle(line, ':before').getPropertyValue('width');
+    a = Math.floor((parseInt(a) / 10) * 2);
+    text.innerHTML = a + '%';
+    console.log(a);
+    if(a == 100){
+        clearInterval(op);
+    }
+}
